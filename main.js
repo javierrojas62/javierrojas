5fbbd358f35416860096d34eb4af5096ced0d6ee
Javier <javierrojas62@gmail.com>
2025-04-01 00:15:00 -0300

    Modificaciones

--- main.js
+++ main.js
@@ -5,20 +5,14 @@ const translations = {
         var2: "About",
         var3: "Contact",
         subtext01: "Technologies ⬇️",
-        subtext02: "My certificates ⬇️",
-        cer01: "Web development",
-        cer02: "Backend web programming",
-        title_01: ["Hello, my name is Javier Rojas. I am a student at the National University of San Juan, pursuing a Web Programming Technician degree with 40% progress. I am developing skills in web programming, database management, and technical problem-solving. I am seeking opportunities to grow professionally and contribute value to technological projects."],
+        title_01: ["Hello, my name is Javier Rojas. I am a student at the National University of San Juan, pursuing a Web Programming Technician degree with 50% progress. I am developing skills in web programming, database management, and technical problem-solving. I am seeking opportunities to grow professionally and contribute value to technological projects."],
     },
     es: {
         var1: "Inicio",
         var2: "Acerca de",
         var3: "Contacto",
         subtext01: "Tecnologías ⬇️",
-        cer01: "Desarrollo Web",
-        cer02: "Programación Web Backend",
-        subtext02: "Mis Certificados ⬇️",
-        title_01: ["Hola, mi nombre es Javier Rojas. Soy estudiante de la Universidad Nacional de San Juan en la carrera de Tecnicatura en Programación Web, con un avance del 40%. Estoy desarrollando habilidades en programación web, manejo de bases de datos y resolución de problemas técnicos. Busco oportunidades para crecer profesionalmente y aportar valor a proyectos tecnológicos."],
+        title_01: ["Hola, mi nombre es Javier Rojas. Soy estudiante de la Universidad Nacional de San Juan en la carrera de Tecnicatura en Programación Web, con un avance del 50%. Estoy desarrollando habilidades en programación web, manejo de bases de datos y resolución de problemas técnicos. Busco oportunidades para crecer profesionalmente y aportar valor a proyectos tecnológicos."],
     },
 };
 
@@ -31,9 +25,6 @@ const elements = {
     title_01: document.getElementById("title_01"),
     subtext01: document.getElementById("subtext01"),
     subtext02: document.getElementById("subtext02"),
-    cer01: document.getElementById("cer01"),
-    cer02: document.getElementById("cer02"),
-
 };
 
 // Variable para almacenar la instancia de Typed.js
@@ -74,19 +65,7 @@ langSwitch.addEventListener("change", () => {
 // Establecer el idioma inicial
 updateLanguage("es");
 
-//certificados
-// Mostrar el certificado en el overlay
-function showCert(imageSrc) {
-    const overlay = document.getElementById('cert-overlay'); // Selecciona el overlay
-    const largeImage = document.getElementById('cert-large'); // Selecciona la imagen grande
-    largeImage.src = imageSrc; // Asigna el `src` recibido a la imagen grande
-    overlay.style.display = 'flex'; // Hace visible el overlay
-  }
-  
-  function closeCert() {
-    const overlay = document.getElementById('cert-overlay');
-    overlay.style.display = 'none'; // Oculta el overlay
-  }
+
   
   //redes
   // Seleccionamos el ícono principal
